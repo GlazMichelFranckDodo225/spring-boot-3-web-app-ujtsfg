9a1fbbafe6c2ffddff33135fa076edfe62d01620
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-06-01 09:55:30 +0200

    Create Author and Book JPA Entities

---
+++ src/main/java/com/dgmf/entity/Author.java
@@ -0,0 +1,19 @@
+package com.dgmf.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+public class Author {
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+}
